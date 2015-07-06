77e835ec2f11b975ea2fa4f61bdac154a9e5c218
Ludo Bermejo <LudoBermejo@gmail.com>
2015-07-06 22:38:26 +0200

    Remove bower_components and dist from the code

--- dist/js/main.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";angular.module("BoilerApp",["ngRoute","ngAnimate"]).config(["$locationProvider","$routeProvider",function(e,o){e.hashPrefix("!"),o.when("/",{templateUrl:"./templates/partial1.html",controller:"MainController"}).otherwise({redirectTo:"/"})}]),angular.module("BoilerApp").controller("MainController",["$scope",function(e){e.test="Testing..."}])}();
