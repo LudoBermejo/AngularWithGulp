5c07decf7d9dab3704128348623bd132c246abf8
Ludo Bermejo <LudoBermejo@gmail.com>
2015-07-15 22:41:41 +0200

    Adding coverage to karma

--- karma.conf.js
+++ karma.conf.js
@@ -32,13 +32,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'app/js/*.js': ['coverage'],
+      'app/modules/*.js': ['coverage'],
+      'app/modules/**/*.js': ['coverage']
     },
 
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress','coverage'],
 
 
     // web server port
